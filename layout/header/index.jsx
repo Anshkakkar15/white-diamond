78c22314d50d29d2f0c71c9f20b18046a74ca4d8
anshkakkar15 <kakkaransh40@gmail.com>
2023-10-15 03:31:41 -0700

    working with footer

--- layout/header/index.jsx
+++ layout/header/index.jsx
@@ -1,48 +1,66 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "@/styles/Layout.module.css";
+import { Menu, X } from "lucide-react";
 
 export const Header = () => {
   const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    if (isActive) {
+      document.body.addEventListener("mousedown", () => setIsActive(false));
+      document.body.style.overflowY = "hidden";
+    } else return;
+
+    return () => {
+      document.body.removeEventListener("mousedown", () => setIsActive(false));
+      document.body.style.overflowY = "auto";
+    };
+  }, [isActive]);
+
   return (
-    <div className={styles.nav_wrapper}>
-      <div className={styles.grad_bar}></div>
-      <nav className={styles.navigationbar}>
-        {/* <img
+    <div
+      className={`${styles.nav_wrapper} ${
+        isActive ? styles.nav_overlay : null
+      }`}
+    >
+      <div className="container-fluid">
+        <div className={styles.grad_bar}></div>
+        <div className={styles.navigationbar}>
+          {/* <img
           src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c8/Bluestar_%28bus_company%29_logo.svg/1280px-Bluestar_%28bus_company%29_logo.svg.png"
           alt="Company Logo"
         /> */}
-        <h2>Logo</h2>
-        <div
-          onClick={() => setIsActive(!isActive)}
-          className={`${styles.menu_toggle} ${
-            isActive ? styles.is_active : null
-          }`}
-          id={styles.mobile_menu}
-        >
-          <span className={styles.bar}></span>
-          <span className={styles.bar}></span>
-          <span className={styles.bar}></span>
+          <h2>Logo</h2>
+          <div
+            onClick={() => setIsActive(true)}
+            className={`${styles.menu_toggle}`}
+          >
+            <Menu />
+          </div>
+          <ul className={`nav ${isActive ? styles.nav_active : null}`}>
+            <li onClick={() => setIsActive(false)} className={styles.cross}>
+              <X color="#BCBCBC" />
+            </li>
+            <li className={styles.nav_items}>
+              <a href="#">Home</a>
+            </li>
+            <li className={styles.nav_items}>
+              <a href="#">About</a>
+            </li>
+            <li className={styles.nav_items}>
+              <a href="#">Work</a>
+            </li>
+            <li className={styles.nav_items}>
+              <a href="#">Services</a>
+            </li>
+            <li className={styles.nav_items}>
+              <a href="#">Contact Us</a>
+            </li>
+          </ul>
         </div>
-        <ul className={`nav ${isActive ? styles.nav_active : null}`}>
-          <li className={styles.nav_items}>
-            <a href="#">Home</a>
-          </li>
-          <li className={styles.nav_items}>
-            <a href="#">About</a>
-          </li>
-          <li className={styles.nav_items}>
-            <a href="#">Work</a>
-          </li>
-          <li className={styles.nav_items}>
-            <a href="#">Careers</a>
-          </li>
-          <li className={styles.nav_items}>
-            <a href="#">Contact Us</a>
-          </li>
-        </ul>
-      </nav>
+      </div>
     </div>
   );
 };
