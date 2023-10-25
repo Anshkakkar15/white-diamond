54a93353eec427df1d6be0b6c78c018f593b66db
Anshkakkar15 <kakkaransh40@gmail.com>
2023-10-25 10:27:28 +0530

    added our mission

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/Button";
 import { ServiceCard } from "@/components/cards/Service";
 
 import styles from "@/styles/Home.module.css";
+import { HandIcon, Pencil, Send } from "lucide-react";
 
 export default function Home() {
   return (
@@ -28,7 +29,7 @@ export default function Home() {
           <div className="row">
             <div className={`col-md-6 ${styles.service_head}`}>
               <h2>
-                OUR <span>SERVICES</span>{" "}
+                OUR <span>SERVICES</span>
               </h2>
             </div>
             <div className={`col-md-6 ${styles.service_content}`}>
@@ -44,6 +45,107 @@ export default function Home() {
             <ServiceCard />
             <ServiceCard />
             <ServiceCard />
+          </div>
+        </div>
+      </div>
+      <div className={`${styles.section_all} bg-light`} id="about">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className={`${styles.section_title_all} text-center`}>
+                <h3>
+                  OUR <span className={styles.text_custom}>MISSION</span>
+                </h3>
+                <p className={`${styles.section_subtitle} mx-auto text-muted`}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row align-items-center mt-3">
+            <div className="col-lg-6">
+              <div className={`${styles.about_header_main} mt-3`}>
+                <div className="about_icon_box">
+                  <p className={`${styles.text_custom} ${styles.abt_head}`}>
+                    Lorem Ipsum is simply dummy text
+                  </p>
+                </div>
+                <h4 className={`${styles.about_heading} text-capitalize`}>
+                  Lorem Ipsum is simply dummy text of the printing industry.
+                </h4>
+                <p className={styles.about_text}>
+                  Contrary to popular belief, Lorem Ipsum is not simply random
+                  text. It has roots in a piece of classical Latin literature
+                  from 45 BC, making it over 2000 years old.
+                </p>
+
+                <p className={styles.about_text}>
+                  Richard McClintock, a Latin professor at Hampden-Sydney
+                  College in Virginia, looked up one of the more obscure Latin
+                  words, consectetur, from a Lorem Ipsum passage.
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className={`${styles.img_about} mt-3`}>
+                <img
+                  src="https://i.ibb.co/qpz1hvM/About-us.jpg"
+                  alt=""
+                  className="img-fluid mx-auto d-block"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="row mt-3">
+            <div className="col-lg-4">
+              <div className={styles.about_content_box_all}>
+                <div className="about_detail text-center">
+                  <div className={styles.about_icon}>
+                    <Pencil />
+                  </div>
+                  <h5 className={styles.about_icon_head}>Creative Design</h5>
+                  <p className={styles.about_icon_desc}>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4">
+              <div className={styles.about_content_box_all}>
+                <div className="about_detail text-center">
+                  <div className={styles.about_icon}>
+                    <HandIcon />
+                  </div>
+                  <h5 className={styles.about_icon_head}>
+                    We make Best Result
+                  </h5>
+                  <p className={styles.about_icon_desc}>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4">
+              <div className={styles.about_content_box_all}>
+                <div className="about_detail text-center">
+                  <div className={styles.about_icon}>
+                    <Send />
+                  </div>
+                  <h5 className={styles.about_icon_head}>best platform </h5>
+                  <p className={styles.about_icon_desc}>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
