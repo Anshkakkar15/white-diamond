a8a0624280e1e6ae4dddb8d6d8018016ee030825
Anshkakkar15 <kakkaransh40@gmail.com>
2023-10-26 10:37:44 +0530

    contact page done

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -9,6 +9,43 @@ export default function Contact() {
   return (
     <main>
       <Banner heading={"Contact Us"} />
+      <div class="container-fluid">
+        <div class={styles.screen}>
+          <div class={styles.screen_body}>
+            <div class={`${styles.screen_body_item} ${styles.left}`}>
+              <div class={styles.app_title}>
+                <span>CONTACT US</span>
+              </div>
+            </div>
+            <div class={styles.screen_body_item}>
+              <div class={styles.app_form}>
+                <div class={styles.app_form_group}>
+                  <input class={styles.app_form_control} placeholder="NAME" />
+                </div>
+                <div class={styles.app_form_group}>
+                  <input class={styles.app_form_control} placeholder="EMAIL" />
+                </div>
+                <div class={styles.app_form_group}>
+                  <input
+                    class={styles.app_form_control}
+                    placeholder="CONTACT NO"
+                  />
+                </div>
+                <div class={`${styles.app_form_group} ${styles.message}`}>
+                  <input
+                    class={styles.app_form_control}
+                    placeholder="MESSAGE"
+                  />
+                </div>
+                <div class={`${styles.app_form_group} ${styles.buttons}`}>
+                  <button class={styles.app_form_button}>SEND</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <section className={styles.map_sec}>
         <Map styles={styles} />
       </section>
